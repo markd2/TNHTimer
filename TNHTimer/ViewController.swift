0a7300eb3f9e33629ee34b68cd0b369c65d188c6
Mark Dalrymple <markd@bignerdranch.com>
2017-07-04 20:52:52 -0400

    Adds timer logic.

--- TNHTimer/ViewController.swift
+++ TNHTimer/ViewController.swift
@@ -9,14 +9,56 @@ class ViewController: UIViewController {
     
     private var isRunning = false
     private var startDate: Date?
+    private var heartbeatTimer: Timer?
     
     private func start() {
         isRunning = true
         startDate = Date()
+        startTimer()
     }
     
     private func stop() {
         isRunning = false
+        stopTimer()
+    }
+    
+    
+    private func startTimer() {
+        // Should not be starting a timer that's already started.
+        precondition(heartbeatTimer == nil)
+        
+        // I run iOS 9 on my precioussss gold 5s.  The nice block version of scheduledTimer came in iOS10.
+        heartbeatTimer = Timer.scheduledTimer(timeInterval: 0.5, 
+                                              target: self, 
+                                              selector: #selector(self.lubdub), 
+                                              userInfo: nil, 
+                                              repeats: true)
+    }
+    
+    private func stopTimer() {
+        // Should not be stopping a timer that's not started
+        precondition(heartbeatTimer != nil)
+        
+        heartbeatTimer?.invalidate()
+        heartbeatTimer = nil
+    }
+    
+
+    // Timer callback
+    @objc private func lubdub() {
+        precondition(startDate != nil)
+        precondition(isRunning)
+        
+        updateElapsedTimeLabel()
+    }
+    
+    private func updateElapsedTimeLabel() {
+        guard let startDate = startDate else {
+            elapsedTimeLabel.text = "0:00:00"
+            return   
+        }
+        let time = abs(startDate.timeIntervalSinceNow)
+        print("time is \(time)")
     }
     
     override func viewDidLoad() {
@@ -30,6 +72,8 @@ class ViewController: UIViewController {
 
         let buttonTitle = isRunning ? "Done" : "Start"
         startDoneButton.setTitle(buttonTitle, for: .normal)
+        
+        updateElapsedTimeLabel()
     }
     
     @IBAction func toggleStartDone() {
